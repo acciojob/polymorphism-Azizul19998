6c10bcdf340cc6d1ccadff0daf5d8f20ec6f06bc
Azizul <you@example.comazizuljohn121454@gmail.com>
2022-10-06 20:03:41 +0530

    second commit/product class made public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,7 @@ public class Main {
         p.product(1.2 , 3.4);
     }
 
-    static class Product {
+    public  static class Product {
         public int product(int x, int y) {
             return x*y;
         }
