ce26544b7ea87427a2f3b78a8bff62fc1efd4dfb
Azizul <you@example.comazizuljohn121454@gmail.com>
2022-10-06 20:09:11 +0530

    third commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,23 +4,23 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Product p = new Product();
+       Product p = new Product();
         p.product(2,3);
         p.product(1,2,3);
         p.product(1.2 , 3.4);
     }
 
-    public  static class Product {
-        public int product(int x, int y) {
-            return x*y;
-        }
-        public int product(int x, int y, int z) {
-            return x*y;
-        }
-        public double product(double x, double y) {
-            return x*y;
-        }
+}
 
+class Product {
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y;
+    }
+    public double product(double x, double y) {
+        return x*y;
     }
 
 }
