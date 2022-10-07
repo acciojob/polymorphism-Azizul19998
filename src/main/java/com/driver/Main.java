a1a5f5572454c7c7ff73cf07c9b6a79a467ad446
Azizul <you@example.comazizuljohn121454@gmail.com>
2022-10-07 21:31:54 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args)
     {
        Product p = new Product();
-        p.product(2,3);
-        p.product(1,2,3);
-        p.product(1.2 , 3.4);
+      int ans1=  p.product(2,3);
+       int ans2 =  p.product(1,2,3);
+       double ans3 =  p.product(1.2 , 3.4);
+        System.out.println(ans1+" "+ans2+" "+ans3);
     }
     public static class Product {
         public int product(int x, int y) {
